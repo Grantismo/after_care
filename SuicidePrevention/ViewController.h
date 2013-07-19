edf3924109a619b2661a598e7aff8df83a8665df
Lucas Best <lucas.best.5@gmail.com>
2013-07-19 16:40:29 -0400

    Initial Commit

---
+++ SuicidePrevention/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SuicidePrevention
+//
+//  Created by Lucas Best on 7/19/13.
+//  Copyright (c) 2013 TabSprint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
