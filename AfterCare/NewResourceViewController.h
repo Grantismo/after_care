6aff66b643724bf0085c1b40b6706874041e572d
grantwarman <grant.warman@gmail.com>
2013-07-21 17:53:48 -0400

    Added new resource controller

---
+++ AfterCare/NewResourceViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewResourceViewController.h
+//  AfterCare
+//
+//  Created by Grant Warman on 7/21/13.
+//  Copyright (c) 2013 TabSprint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewResourceViewController : UITableViewController
+
+@end
